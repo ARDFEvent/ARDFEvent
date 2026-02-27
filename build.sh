9084aa8d7b088f8b33f7974c41766cf1b399acb3
Jakub Jiroutek <jjiroutek6@gmail.com>
2026-02-27 18:10:14 +0100

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 Color_Off='\033[0m'
 BYellow='\033[1;33m'
 BPurple='\033[1;35m'
+BRed='\033[1;31m'
+BGreen='\033[1;32m'
 BIPurple='\033[1;95m'
 BIGreen='\033[1;92m'
 
@@ -16,6 +18,16 @@ source .venv/bin/activate
 echo -e "${BIPurple}Begin essentials build process...${Color_Off}"
 echo -e "${BPurple}Install requirements using pip${Color_Off}"
 pip install -r requirements.txt
+echo -e "${BPurple}Checking for system GDAL...${Color_Off}"
+if ! command -v gdal-config &> /dev/null; then
+    echo -e "${BRed}Error: libgdal-dev is not installed on your system.${Color_Off}"
+    echo -e "${BRed}Please run: sudo apt install libgdal-dev (or your distro's equivalent)${Color_Off}"
+    exit 1
+fi
+GDAL_VERSION=$(gdal-config --version)
+echo -e "${BGreen}Found system GDAL version: $GDAL_VERSION${Color_Off}"
+echo -e "${BIPurple}Installing matching python-gdal bindings...${Color_Off}"
+pip install "gdal==$GDAL_VERSION"
 cd src/rust_results || exit
 echo -e "${BYellow}Install required rust libraries using cargo${Color_Off}"
 cargo fetch
